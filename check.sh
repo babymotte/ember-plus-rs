8aefe080348c727aebf15f1af3c61e6119079cb4
Michael Bachmann <mbachmann@bbmsoft.net>
2025-10-15 10:43:16 +0200

    stricter checks

--- check.sh
+++ check.sh
@@ -4,5 +4,6 @@ cargo build &&
     cargo build --no-default-features &&
     cargo test &&
     cargo test --no-default-features &&
-    cargo fmt &&
-    cargo clippy
+    cargo fmt --check &&
+    cargo clippy -- --deny warnings &&
+    cargo clippy --no-default-features -- --deny warnings
